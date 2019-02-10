1b36bf2da368f17a9fdb71ee6db2708a68aa637c
VIKASH PATHAK <vikashpathak1121@gmail.com>
2019-02-11 00:42:06 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1DeXTyDIsqXaKM4UQCJDJCwd-udnjpZy'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1DeXTyDIsqXaKM4UQCJDJCwd-udnjpZy6'
 export_file_name = 'export.pkl'
 
 classes = ['golf stick','hockey stick']
